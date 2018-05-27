97af6bccf077b302773b868f015a5b4c4143c782
Kamol Roy <kamol.roy9943@gmail.com>
2018-05-27 18:16:16 +0600

    Change the applicatio name, Added move and customar class and show the moview list

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie() {Id = 1, Name = "Blood Diamond"},
+                new Movie() {Id = 2, Name = "Titanic"}
+            };
+            return View(movies);
+        }
+    }
+}
