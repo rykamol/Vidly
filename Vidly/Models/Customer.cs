cbddcd778a163d60321529bfe2f2ce1dfe3b2a33
Kamol Roy <kamol.roy9943@gmail.com>
2018-05-29 23:42:02 +0600

    Add MembershipType and make relation with customer class

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribeToNewsLetter { get; set; }
-
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
---
+++ Vidly/Models/MemberShipType.cs
@@ -0,0 +1,10 @@
+﻿namespace Vidly.Models
+{
+    public class MemberShipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
